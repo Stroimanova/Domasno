a9c2a0596992e13e2afd3fa8bbe491b069d2215c
unknown <simonastroimanova@hotmail.com>
2024-01-20 19:04:59 +0100

    first java program

---
+++ src/proekt51/Fizika.java
@@ -0,0 +1,22 @@
+package proekt51;
+
+public class Fizika {
+	public static void main(String[] args) {
+		System.out.println("Vnesete masa na bakarnoto topce");
+		double masa = 64; // masa vo gramovi
+		System.out.println("Vnesete volumen na bakarnoto topce");
+		double volumen = 10; // volumen vo cm ^3
+		System.out.println("Vnesete gustina na bakarnoto topce");
+		double gustina = 8.9; // gustina vo g/cm^3
+		  //Presmetka na gustinata na topceto
+		double calculatedGustina = masa / volumen;
+		  //Proverka dali gustinata na topceto se sovpaga so gustinata na bakarot
+		if (calculatedGustina == gustina) {
+			System.out.println("Vo bakarnoto topce nema suplina. ");
+		}else{
+			System.out.println("Vo bakarnoto topce ima suplina. ");
+			
+		}
+
+		}
+}
